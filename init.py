e78306686c3286279253c0198de8dd55e7367fb0
utkuufuk <utkuufuk@gmail.com>
2018-11-04 22:02:05 +0300

    remove redundant lines from initialization script

--- init.py
+++ init.py
@@ -1,11 +1,7 @@
-from __future__ import print_function
-from googleapiclient.discovery import build
-from httplib2 import Http
 from oauth2client import file, client, tools
 
 # if modifying these scopes, delete the file token.json
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
-SPREADSHEET_ID = '1jANO8_sbQ5pLEAJbyxWcQiklPboPtSp8ijrp_RTD0Aw'
 
 if __name__ == '__main__':
     store = file.Storage('token.json')
