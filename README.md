1e359c25d60b4790de684e5c5650e9222cfabd31
utkuufuk <utkuufuk@gmail.com>
2018-11-04 21:16:58 +0300

    simplify program arguments

---
+++ README.md
@@ -0,0 +1,8 @@
+# google-budget
+Adds transaction entries to the budget spreadsheet.
+
+## Installation
+``` sh
+pip install --upgrade google-api-python-client oauth2client
+```
+
--- addentry.py
+++ addentry.py
@@ -2,21 +2,14 @@ from __future__ import print_function
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
-import argparse
+import sys
 
-# If modifying these scopes, delete the file token.json.
+# if modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
 SPREADSHEET_ID = '1jANO8_sbQ5pLEAJbyxWcQiklPboPtSp8ijrp_RTD0Aw'
 
 if __name__ == '__main__':
-    # parse program arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--date', type=str, help='transaction date')
-    parser.add_argument('--cost', type=int, help='transaction amount')
-    parser.add_argument('--desc', type=str, help='transaction description')
-    parser.add_argument('--type', type=str, help='transaction type')
-    args = parser.parse_args()
-
+    print("Transaction:", str(sys.argv[1]).split(','))
     store = file.Storage('token.json')
     creds = store.get()
     if not creds or creds.invalid:
@@ -29,14 +22,14 @@ if __name__ == '__main__':
     result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=rangeName).execute()
     values = result.get('values', [])
 
-    # iterate down to the last entry
+    # find the index of the last entry
     index = 5
     if values:
         index += len(values)
 
     # add new entry
     rangeName = "Transactions!B" + str(index) + ":E" + str(index)
-    entry = [[args.date, args.cost, args.desc, args.type]]
+    entry = [sys.argv[1].split(',')]
     body = {'values': entry}
     result = service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID, range=rangeName,
                                                     valueInputOption="USER_ENTERED", body=body).execute()
