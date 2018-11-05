31a2b823df782d171da53b62030353ec8bb096e8
utkuufuk <utkuufuk@gmail.com>
2018-11-05 23:56:38 +0300

    add new command to set spreadsheet ID

--- budget.py
+++ budget.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from __future__ import print_function
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 from httplib2 import Http
 from oauth2client import file, client, tools
 from pathlib import Path
@@ -8,48 +8,67 @@ import sys
 import os
 import shutil
 
-# replace this with your spreadsheet ID
-SPREADSHEET_ID = '1jANO8_sbQ5pLEAJbyxWcQiklPboPtSp8ijrp_RTD0Aw'
 GLOBAL_TOKEN_PATH = str(Path.home()) + '/.local/share/token.json'
+SPREADSHEET_ID_PATH = str(Path.home()) + '/.local/share/spreadsheet.id'
 
 if __name__ == '__main__':
-    # validate input
+    # validate command
     command = sys.argv[1]
     if command == 'expense' or command == 'income':
-        print("Attempting to insert new", command, "entry:")
-    else:
-        print("ERROR: Invalid command. Valid commands are 'expense' and 'income'.")
+        print("Attempting to insert new", command, "transaction:")
+    elif command == 'sheet':
+        # write spreadsheet ID
+        with open(SPREADSHEET_ID_PATH, 'w') as f:
+            f.write(sys.argv[2])
+        print("Spreadsheet ID has been set:", sys.argv[2])
         sys.exit(0)
+    else:
+        print("Invalid command. Valid commands are 'sheet', 'expense' and 'income'.", file=sys.stderr)
+        sys.exit(1)
+
+    # validate transaction
     entry = sys.argv[2].split(',')
     if len(entry) is 4:
         print("Date:", entry[0], "\nAmount:", entry[1], "\nDescription:", entry[2], "\nCategory:", entry[3], "\n")
     else:
-        print("ERROR: Invalid number of fields.")
-        sys.exit(0)
+        print("Invalid number of fields in transaction.", file=sys.stderr)
+        sys.exit(1)
 
-    # authorize
+    # read spreadsheet ID
+    try:
+        with open(SPREADSHEET_ID_PATH) as f:
+            ssheetId = f.read()
+    except:
+        print("Spreadsheet ID not found. Set your spreadsheet ID with the following command:", file=sys.stderr)
+        print("budget sheet <spreadsheet_id>", file=sys.stderr)
+        sys.exit(1)
+
+    # temporarily copy the auth token into current dir and authorize
     shutil.copyfile(GLOBAL_TOKEN_PATH, 'token.json')
     store = file.Storage('token.json')
     creds = store.get()
     service = build('sheets', 'v4', http=creds.authorize(Http()))
     print("Authorization successful.")
 
-    # fetch existing transactions
-    rangeName = 'Transactions!C5:C74' if command == 'expense' else 'Transactions!H5:H74'
-    result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=rangeName).execute()
-    values = result.get('values', [])
+    # fetch existing data in order to find the index of the last transaction
+    try:
+        rangeName = 'Transactions!C5:C74' if command == 'expense' else 'Transactions!H5:H74'
+        result = service.spreadsheets().values().get(spreadsheetId=ssheetId, range=rangeName).execute()
+        values = result.get('values', [])
+        index = 5
+        if values:
+            index += len(values)
+    except HttpError:
+        print("Invalid spreadsheet ID. Set your spreadsheet ID with the following command:", file=sys.stderr)
+        print("budget sheet <spreadsheet_id>", file=sys.stderr)
+        sys.exit(1)
 
-    # find the index of the last entry
-    index = 5
-    if values:
-        index += len(values)
-
-    # add new entry
+    # add new transaction
     startCol = "B" if command == 'expense' else "G"
     endCol = "E" if command == 'expense' else "J"
     rangeName = "Transactions!" + startCol + str(index) + ":" + endCol + str(index)
     body = {'values': [entry]}
-    result = service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID, range=rangeName,
+    result = service.spreadsheets().values().update(spreadsheetId=ssheetId, range=rangeName,
                                                     valueInputOption="USER_ENTERED", body=body).execute()
     print('{0} cells updated.'.format(result.get('updatedCells')))
     os.remove('token.json')
