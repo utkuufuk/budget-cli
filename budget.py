f27921ba8ded7c78bc646afa5770b192af9bf029
utkuufuk <utkuufuk@gmail.com>
2018-11-09 22:43:51 +0300

    validate argument for the url command

--- budget.py
+++ budget.py
@@ -9,26 +9,35 @@ import os
 import datetime
 
 APP_DIR = str(Path.home()) + '/.local/share/budget-cli/'
-GLOBAL_TOKEN_PATH = APP_DIR + 'token.json'
 SPREADSHEET_ID_PATH = APP_DIR + 'spreadsheet.id'
 
 if __name__ == '__main__':
-    # validate command
     command = sys.argv[1]
-    if command == 'url' or command == 'id':
-        arg = sys.argv[2]
-        ssheetId = arg if command == 'id' else arg[(arg.find("spreadsheets/d/") + 15):arg.find("/edit#")]
-        # write spreadsheet ID
+    arg = sys.argv[2]
+
+    # validate URL
+    if command == 'url': 
+        start = arg.find("spreadsheets/d/")
+        end = arg.find("/edit#")
+        if start == -1 or end == -1:
+            print("Invalid URL:", arg, file=sys.stderr)
+            sys.exit(1)
+
+    # write spreadsheet ID & exit if command is 'id' or 'url'
+    if command == 'id' or command == 'url':
+        ssheetId = arg if command == 'id' else arg[(start + 15):end]
         with open(SPREADSHEET_ID_PATH, 'w') as f:
             f.write(ssheetId)
         print("Spreadsheet ID has been set:", ssheetId)
         sys.exit(0)
+
+    # exit if command is invalid
     if command != 'expense' and command != 'income':
-        print("Invalid command. Valid commands are 'id', 'url', 'expense' and 'income'.", file=sys.stderr)
+        print("Invalid command:", command, "\nValid commands are 'id', 'url', 'expense' and 'income'.", file=sys.stderr)
         sys.exit(1)
 
-    # validate transaction
-    entry = sys.argv[2].split(',')
+    # validate transaction if command is 'expense' or 'income'
+    entry = arg.split(',')
     if len(entry) != 3 and len(entry) != 4:
         print("Invalid number of fields in transaction.", file=sys.stderr)
         sys.exit(1)
@@ -38,7 +47,7 @@ if __name__ == '__main__':
         entry.insert(0, str(now)[:10])
     print("Date:", entry[0], "\nAmount:", entry[1], "\nDescription:", entry[2], "\nCategory:", entry[3], "\n")
 
-    # read spreadsheet ID
+    # read spreadsheet ID from file
     try:
         with open(SPREADSHEET_ID_PATH) as f:
             ssheetId = f.read()
@@ -47,7 +56,7 @@ if __name__ == '__main__':
         print("budget id <SPREADSHEET_ID>\nbudget url <SPREADSHEET_URL>", file=sys.stderr)
         sys.exit(1)
 
-    # temporarily change working directory to read token.json and authorize
+    # temporarily change working directory to read token.json & authorize
     initialDir = os.getcwd()
     os.chdir(APP_DIR)
     store = file.Storage('token.json')
@@ -56,12 +65,12 @@ if __name__ == '__main__':
     print("Authorization successful.")
     os.chdir(initialDir)
 
-    # fetch existing data in order to find the index of the last transaction
+    # fetch existing data in order to find the row index of the last transaction
     try:
         rangeName = 'Transactions!C5:C74' if command == 'expense' else 'Transactions!H5:H74'
         result = service.spreadsheets().values().get(spreadsheetId=ssheetId, range=rangeName).execute()
         values = result.get('values', [])
-        index = 5 if not values else 5 + len(values)
+        rowIdx = 5 if not values else 5 + len(values)
     except HttpError:
         print("Invalid spreadsheet ID: " + ssheetId, file=sys.stderr)
         print("Set your spreadsheet ID with the following command:\nbudget sheet <spreadsheet_id>", file=sys.stderr)
@@ -70,7 +79,7 @@ if __name__ == '__main__':
     # add new transaction
     startCol = "B" if command == 'expense' else "G"
     endCol = "E" if command == 'expense' else "J"
-    rangeName = "Transactions!" + startCol + str(index) + ":" + endCol + str(index)
+    rangeName = "Transactions!" + startCol + str(rowIdx) + ":" + endCol + str(rowIdx)
     body = {'values': [entry]}
     result = service.spreadsheets().values().update(spreadsheetId=ssheetId, range=rangeName,
                                                     valueInputOption="USER_ENTERED", body=body).execute()
