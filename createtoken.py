ea94883312257dbd1f6ecb00896c855e164cdb6e
utkuufuk <utkuufuk@gmail.com>
2018-12-09 15:28:40 +0300

    polish token creation script

--- createtoken.py
+++ createtoken.py
@@ -2,12 +2,9 @@ from oauth2client import file, client, tools
 from httplib2 import Http
 from googleapiclient.discovery import build
 
-# delete 'token.json' if this is modified
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
-
-# create authorization token from the 'credentials.json' file
 store = file.Storage('token.json')
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+    scopes = 'https://www.googleapis.com/auth/spreadsheets'
+    flow = client.flow_from_clientsecrets('credentials.json', scopes)
     tools.run_flow(flow, store)
